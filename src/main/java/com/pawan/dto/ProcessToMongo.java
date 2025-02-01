3492763d6aae7d9b2df20174b96e48f73c48b25e
PawanWpits <pawan@wpitservices.com>
2025-02-01 17:25:48 +0530

    Fourth commit

--- src/main/java/com/pawan/dto/ProcessToMongo.java
+++ src/main/java/com/pawan/dto/ProcessToMongo.java
@@ -1,7 +1,11 @@
 package com.pawan.dto;
 
+import org.springframework.http.HttpStatus;
+
 public class ProcessToMongo {
 
     private int id;
     private String processId;
+
+    private HttpStatus status;
 }
